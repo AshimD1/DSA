f6049fa2814946ed0584d92eaae561f75058bbf9
Ashim Dongol <85755127+AshimD1@users.noreply.github.com>
2022-09-07 09:27:06 +0530

    Add files via upload

---
+++ String/Sorting a String/sortingString.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+    string s1 = "rnitgos";
+    sort(s1.begin(),s1.end());
+    cout<<s1;
+
+    return 0;
+}
