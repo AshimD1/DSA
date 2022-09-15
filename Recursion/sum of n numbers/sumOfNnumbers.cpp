07baf80e8c1f03010b2070b1d892cf3387a504c8
Ashim Dongol <85755127+AshimD1@users.noreply.github.com>
2022-09-15 11:15:46 +0530

    Add files via upload

---
+++ Recursion/sum of n numbers/sumOfNnumbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int Sum(int n){
+    if(n==0){
+        return 0;
+    }
+    int prevsum = Sum(n-1);
+    return n+ prevsum;
+}
+int main() {
+
+    int n;
+    cin>>n;
+
+    cout<<Sum(n);
+    return 0;
+}
